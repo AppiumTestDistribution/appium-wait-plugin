a498c9543b2e4846427c61de2acda2253fde135e
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2021-03-10 14:07:39 +0530

    remove retries and try polling using node fetch

--- src/plugin.js
+++ src/plugin.js
@@ -1,27 +1,26 @@
 import BasePlugin from '@appium/base-plugin';
+import fetch from 'node-fetch';
 const retry = require('async-retry');
 
 export default class WaitCommandPlugin extends BasePlugin {
   async findElement(next, driver, ...args) {
-    await retry(
-      async (bail) => {
-        console.log(
-          `Before findElement is run with args ${JSON.stringify(args)}`
-        );
-        let originalRes;
-        try {
-          originalRes = await next();
-        } catch (err) {
-          console.log('In plugin', err);
-          throw new Error(err);
-        }
-        console.log(`After findElement is run`, originalRes);
-        return originalRes;
-      },
+    console.log(`Before findElement is run with args ${JSON.stringify(args)}`);
+    let originalRes;
+    const response = await fetch(
+      `http://localhost:8200/wd/hub/session/${driver.sessionId}/element`,
       {
-        retries: 5,
-        factor: 1,
+        body: JSON.stringify({ using: 'accessibility id', value: 'Views' }),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
       }
     );
+    const json = await response.json();
+    console.log('====================================');
+    console.log(json);
+    console.log('====================================');
+    originalRes = await next();
+    console.log('After findElement is run', originalRes);
+    console.log('driver details', driver);
+    return originalRes;
   }
 }
