dd998d4f2549833d9d089e8ef0d6d968a0b70151
saikrishna321 <saikrishna321@yahoo.com>
2021-03-12 00:18:35 +0530

    Consider active driver port and host

--- src/plugin.js
+++ src/plugin.js
@@ -6,17 +6,15 @@ export default class WaitCommandPlugin extends BasePlugin {
   async findElement(next, driver, ...args) {
     const locatorArgs = JSON.parse(JSON.stringify(args));
     let originalRes;
-    try {
-      await this._find(driver, locatorArgs);
-    } catch (e) {
-      originalRes = await next();
-    }
+    await this._find(driver, locatorArgs);
+    originalRes = await next();
+    retryCount = 0;
     return originalRes;
   }
 
   async _find(driver, locatorArgs) {
     const response = await fetch(
-      `http://localhost:8200/wd/hub/session/${driver.sessionId}/element`,
+      `http://${driver.uiautomator2.host}:${driver.uiautomator2.systemPort}/wd/hub/session/${driver.sessionId}/element`,
       {
         body: JSON.stringify({
           strategy: locatorArgs[0],
@@ -29,12 +27,11 @@ export default class WaitCommandPlugin extends BasePlugin {
       }
     );
     const json = await response.json();
-    console.log(json);
-    if (retryCount === 5) {
+    if (retryCount === 25) {
       throw new Error('Element not found...');
     }
     if (json.value.error) {
-      console.log(json.value.error, 'Retying...');
+      console.log(json.value.error, 'Retying...', retryCount);
       retryCount++;
       await this._find(driver, locatorArgs);
     }
