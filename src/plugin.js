29c9ae4be10119848c2e7a1e8bb24e4559929aaf
saikrishna321 <saikrishna321@yahoo.com>
2021-03-11 08:58:35 +0530

    Retry when element is not found!

--- src/plugin.js
+++ src/plugin.js
@@ -1,26 +1,42 @@
 import BasePlugin from '@appium/base-plugin';
 import fetch from 'node-fetch';
-const retry = require('async-retry');
 
+let retryCount = 0;
 export default class WaitCommandPlugin extends BasePlugin {
   async findElement(next, driver, ...args) {
-    console.log(`Before findElement is run with args ${JSON.stringify(args)}`);
+    const locatorArgs = JSON.parse(JSON.stringify(args));
     let originalRes;
+    try {
+      await this._find(driver, locatorArgs);
+    } catch (e) {
+      originalRes = await next();
+    }
+    return originalRes;
+  }
+
+  async _find(driver, locatorArgs) {
     const response = await fetch(
       `http://localhost:8200/wd/hub/session/${driver.sessionId}/element`,
       {
-        body: JSON.stringify({ using: 'accessibility id', value: 'Views' }),
+        body: JSON.stringify({
+          strategy: locatorArgs[0],
+          selector: locatorArgs[1],
+          context: '',
+          multiple: false,
+        }),
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
       }
     );
     const json = await response.json();
-    console.log('====================================');
     console.log(json);
-    console.log('====================================');
-    originalRes = await next();
-    console.log('After findElement is run', originalRes);
-    console.log('driver details', driver);
-    return originalRes;
+    if (retryCount === 5) {
+      throw new Error('Element not found...');
+    }
+    if (json.value.error) {
+      console.log(json.value.error, 'Retying...');
+      retryCount++;
+      await this._find(driver, locatorArgs);
+    }
   }
 }
