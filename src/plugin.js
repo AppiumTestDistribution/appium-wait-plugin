7ed0a73e69cf22b64bb1c603ab5da5b959d10f5f
saikrishna321 <saikrishna321@yahoo.com>
2021-03-12 08:15:18 +0530

    allow default driver to return element state

--- src/plugin.js
+++ src/plugin.js
@@ -6,13 +6,13 @@ export default class WaitCommandPlugin extends BasePlugin {
   async findElement(next, driver, ...args) {
     const locatorArgs = JSON.parse(JSON.stringify(args));
     let originalRes;
-    await this._find(driver, locatorArgs);
+    await this._find(driver, locatorArgs, next);
     originalRes = await next();
     retryCount = 0;
     return originalRes;
   }
 
-  async _find(driver, locatorArgs) {
+  async _find(driver, locatorArgs, next) {
     const response = await fetch(
       `http://${driver.uiautomator2.host}:${driver.uiautomator2.systemPort}/wd/hub/session/${driver.sessionId}/element`,
       {
@@ -27,13 +27,12 @@ export default class WaitCommandPlugin extends BasePlugin {
       }
     );
     const json = await response.json();
-    if (retryCount === 25) {
-      throw new Error('Element not found...');
-    }
     if (json.value.error) {
-      console.log(json.value.error, 'Retying...', retryCount);
-      retryCount++;
-      await this._find(driver, locatorArgs);
+      if (retryCount !== 25) {
+        console.log(json.value.error, 'Retying...', retryCount);
+        retryCount++;
+        await this._find(driver, locatorArgs);
+      }
     }
   }
 }
