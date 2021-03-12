e42abc1daa1eb1f62f911ae1c02efedc4041a0cd
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2021-03-12 13:26:46 +0530

    log waits from appium support library

--- src/plugin.js
+++ src/plugin.js
@@ -6,19 +6,22 @@ export default class WaitCommandPlugin extends BasePlugin {
   async findElement(next, driver, ...args) {
     const locatorArgs = JSON.parse(JSON.stringify(args));
     let originalRes;
-    await this._find(driver, locatorArgs, next);
+    await this._find(driver, locatorArgs);
     originalRes = await next();
     retryCount = 0;
     return originalRes;
   }
 
-  async _find(driver, locatorArgs, next) {
+  async _find(driver, locatorArgs) {
+    const strategy = locatorArgs[0];
+    const selector = locatorArgs[1];
+    const automationName = this._getAutomationName(driver);
     const response = await fetch(
-      `http://${driver.uiautomator2.host}:${driver.uiautomator2.systemPort}/wd/hub/session/${driver.sessionId}/element`,
+      `http://${driver[automationName].host}:${driver[automationName].systemPort}/wd/hub/session/${driver.sessionId}/element`,
       {
         body: JSON.stringify({
-          strategy: locatorArgs[0],
-          selector: locatorArgs[1],
+          strategy,
+          selector,
           context: '',
           multiple: false,
         }),
@@ -29,10 +32,16 @@ export default class WaitCommandPlugin extends BasePlugin {
     const json = await response.json();
     if (json.value.error) {
       if (retryCount !== 25) {
-        console.log(json.value.error, 'Retying...', retryCount);
+        this.logger.info(
+          `Retrying to find element with ${strategy} strategy for ${selector} selector`
+        );
         retryCount++;
         await this._find(driver, locatorArgs);
       }
     }
+  }
+
+  _getAutomationName(driver) {
+    return driver.caps.automationName.toLowerCase();
   }
 }
